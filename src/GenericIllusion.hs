09fa44abbb983261911fb73e3047c3cca0982224
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2019-10-20 10:07:59 +0300

    Implement POC MergeRecords

--- src/GenericIllusion.hs
+++ src/GenericIllusion.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module GenericIllusion
-       ( someFunc
+       ( MergeRecords (..)
        ) where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import GHC.Generics
+import           Data.Coerce                    ( Coercible, coerce )
+
+data MergeRecords a b = MR a b
+
+type GIMetaData = 'MetaData "Generic_Illusion" "Generic_Illusion" "Generic_Illusion" 'False
+type GIMetaCons = 'MetaCons "MR" 'PrefixI 'True
+
+type SimpleRecord a = (Generic a, Coercible (Rep a) (Strip (Rep a)))
+
+instance (SimpleRecord a, SimpleRecord b) => Generic (MergeRecords a b) where
+    type Rep (MergeRecords a b) =
+      D1 GIMetaData (C1 GIMetaCons (Strip (Rep a) :*: (Strip (Rep b))))
+
+    from :: forall x. MergeRecords a b -> Rep (MergeRecords a b) x
+    from (MR a b) = coerce
+        $ (coerce $ (from a :: Rep a x) :: Strip (Rep a) x)
+      :*: (coerce $ (from b :: Rep b x) :: Strip (Rep b) x)
+
+    to :: forall x. Rep (MergeRecords a b) x -> MergeRecords a b
+    to (M1 (M1 (ra :*: rb))) = MR
+      (to (coerce ra :: Rep a x))
+      (to (coerce rb :: Rep b x))
+
+type family Strip (r :: * -> *) :: * -> * where
+  Strip (D1 _ (C1 _ fields)) = fields
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,46 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main (main) where
 
+import Data.Aeson as A
+
+import GenericIllusion
+
+import GHC.Generics
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+data A = A
+  { field1 :: Int
+  , field2 :: String
+  } deriving (Generic)
+
+data B = B
+  { field3 :: Int
+  , field4 :: String
+  } deriving (Generic)
+
+newtype AB = AB (MergeRecords A B)
+  deriving (Generic) via (MergeRecords A B)
+  deriving (A.ToJSON, A.FromJSON)
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [recordTests]
+
+recordTests :: TestTree
+recordTests = testCase "JSON Merge" $
+  A.toJSON (AB $ MR (A 1 "2") (B 3 "4")) @?= A.object
+    [ "field1" .= (1 :: Int)
+    , "field2" .= ("2" :: String)
+    , "field3" .= (3 :: Int)
+    , "field4" .= ("4" :: String)
+    ]
